6709e712d4e6fa8f1f4f2f8125d6f821213c7fa1
dezadkins <desmondadkins@gmail.com>
2020-11-12 17:22:14 -0500

    Beginning Phase 5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import ClassComponent from './class-component/ClassComponent';
 import HeadingA from './css-modules/HeadingA';
 import HeadingB from './css-modules/HeadingB';
 import Image from './image/Image'
 
 function App() {
   return (
-    <div>
-      <h1>exploring React Builds</h1>
-      <HeadingA />
-      <HeadingB />
-      <Image />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>{process.env.REACT_APP_TITLE}</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/image">Image</Link>
+            </li>
+            <li>
+              <Link to="/class-component">Class Component</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/image">
+            <Image />
+          </Route>
+          <Route path="/class-component">
+            <ClassComponent />
+          </Route>
+          <Route path="/">
+            <HeadingA />
+            <HeadingB />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
